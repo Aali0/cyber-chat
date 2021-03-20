93ed678722792d239164982bd9910f9f3027feb3
Ahmed Ali <ahmed.adil.ali@gmail.com>
2021-03-20 14:47:39 -0700

    further devloped translation functionality

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react';
 import { sendMessage, isTyping } from 'react-chat-engine';
 import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 import  TranslateApi  from '../utils/API';
-
+import {Form} from 'react-bootstrap';
 
 const MessageForm = (props) => {
+
     const [value, setValue] = useState('');
     const { chatId, creds } = props;
+    const [language, setLanguage] = useState('');
+    const [message, setMessage] = useState('');
+
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -16,16 +20,30 @@ const MessageForm = (props) => {
 
         if(text.length > 0) sendMessage(creds, chatId, { text });
 
-        TranslateApi(text);
+        TranslateApi(text, language);
 
-         setValue('');
+        setMessage(TranslateApi(text, language));
+        // console log for async output
+        console.log(TranslateApi(text, language));
+
+        setValue('');
+
     }
 
     const handleChange = (event) => {
         setValue(event.target.value);
 
         isTyping(props, chatId);
+    }
 
+    const handleLangChange = (e) => {
+        e.preventDefault();
+
+        const languageChoice = document.querySelector(".language");
+
+        setLanguage(languageChoice.value)
+
+        console.log(language);
     }
 
     const handleUpload = (event) => {
@@ -58,6 +76,13 @@ const MessageForm = (props) => {
             <button type="submit" className="send-button">
                 <SendOutlined className="send-icon" />
             </button>
+            <Form.Control as="select" className="language" onChange={handleLangChange}>
+            <option value="en">English</option>
+            <option value="es">Spanish</option>
+            <option value="de">German</option>
+            <option value="ko">Korean</option>
+            <option value="ar">Arabic</option>
+            </Form.Control>
         </form>
         
     );
