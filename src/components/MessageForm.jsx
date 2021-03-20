6642f8f5e2f52d193f5b84e4c53f02285e2fc069
Rinzler8806 <shane.rogers916@gmail.com>
2021-03-20 09:12:07 -0800

    Added language selection drop-down

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -2,6 +2,14 @@ import { useState } from 'react';
 import { sendMessage, isTyping } from 'react-chat-engine';
 import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 import  TranslateApi  from '../utils/API';
+// import "../node_modules/jquery/dist/jquery.min.js";
+// import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import FormControl from "react-bootstrap/FormControl";
 
 const MessageForm = (props) => {
     const [value, setValue] = useState('');
@@ -55,6 +63,24 @@ const MessageForm = (props) => {
             <button type="submit" className="send-button">
                 <SendOutlined className="send-icon" />
             </button>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <NavDropdown title="Select Language" id="responsive-navbar-nav">
+        <NavDropdown.Item href="#action/3.1">English</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.2">Spanish</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.3">German</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.3">Korean</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.3">Arabic</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
         </form>
     );
 }
