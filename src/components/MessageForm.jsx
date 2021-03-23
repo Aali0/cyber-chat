d86b78c3d04b259f39f6ad56b1fc93d4b2a2c8b6
Ahmed Ali <ahmed.adil.ali@gmail.com>
2021-03-22 21:39:51 -0700

    functioning translation

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,91 +1,87 @@
-import { useState } from 'react';
-import { sendMessage, isTyping } from 'react-chat-engine';
-import { SendOutlined, PictureOutlined } from '@ant-design/icons';
-import  TranslateApi  from '../utils/API';
-import {Form} from 'react-bootstrap';
+import { useState } from "react";
+import { sendMessage, isTyping } from "react-chat-engine";
+import { SendOutlined, PictureOutlined } from "@ant-design/icons";
+import TranslateApi from "../utils/API";
+import { Form } from "react-bootstrap";
 
 const MessageForm = (props) => {
+  const [value, setValue] = useState("");
+  const { chatId, creds } = props;
+  const [language, setLanguage] = useState("");
+  const [translated, setTranslated] = useState("");
 
-    const [value, setValue] = useState('');
-    const { chatId, creds } = props;
-    const [language, setLanguage] = useState('');
-    const [message, setMessage] = useState('');
-
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        const text = value.trim();
-        console.log(text);
-
-        if(text.length > 0) sendMessage(creds, chatId, { text });
-
-        TranslateApi(text, language);
-
-        setMessage(TranslateApi(text, language));
-        // console log for async output
-        console.log(TranslateApi(text, language));
-
-        setValue('');
-
-    }
-
-    const handleChange = (event) => {
-        setValue(event.target.value);
-
-        isTyping(props, chatId);
-    }
-
-    const handleLangChange = (e) => {
-        e.preventDefault();
-
-        const languageChoice = document.querySelector(".language");
-
-        setLanguage(languageChoice.value)
-
-        console.log(language);
-    }
-
-    const handleUpload = (event) => {
-        sendMessage(creds, chatId, { files: event.target.files, text: ''});
-    }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // const text = TranslateApi(text, language);
+    const text = value.trim();
 
 
+    console.log("text:  " + text);
 
-    return (
-        <form className="message-form" onSubmit={handleSubmit}>
-            <input
-                className="message-input"
-                placeholder="Send a message..."
-                value={value}
-                onChange={handleChange} 
-                onSubmit={handleSubmit}
-            />
-            <label htmlFor="upload-button">
-                <span className="image-button">
-                    <PictureOutlined  className="picture-icon" />
-                </span>
-            </label>
-            <input 
-                type="file"
-                multiple={false}
-                id="upload-button"
-                style={{ display: 'none' }}
-                onChange={handleUpload}
-            />
-            <button type="submit" className="send-button">
-                <SendOutlined className="send-icon" />
-            </button>
-            <Form.Control as="select" className="language" onChange={handleLangChange}>
-            <option value="en">English</option>
-            <option value="es">Spanish</option>
-            <option value="de">German</option>
-            <option value="ko">Korean</option>
-            <option value="ar">Arabic</option>
-            </Form.Control>
-        </form>
-        
-    );
-}
+    const translated = await TranslateApi(text, language)
+    setTranslated(translated);
+    if (text.length > 0) sendMessage(creds, chatId, { text: translated });
+    console.log(`API :${translated}`);
+    setValue("");
+  };
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+
+    isTyping(props, chatId);
+  };
+
+  const handleLangChange = (e) => {
+    e.preventDefault();
+
+    const languageChoice = document.querySelector(".language");
+
+    setLanguage(languageChoice.value);
+
+    console.log(language);
+  };
+
+  const handleUpload = (event) => {
+    sendMessage(creds, chatId, { files: event.target.files, text: translated });
+  };
+
+  return (
+    <form className="message-form" onSubmit={handleSubmit}>
+      <input
+        className="message-input"
+        placeholder="Send a message..."
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+      <label htmlFor="upload-button">
+        <span className="image-button">
+          <PictureOutlined className="picture-icon" />
+        </span>
+      </label>
+      <input
+        type="file"
+        multiple={false}
+        id="upload-button"
+        style={{ display: "none" }}
+        onChange={handleUpload}
+      />
+      <button type="submit" className="send-button">
+        <SendOutlined className="send-icon" />
+      </button>
+      <Form.Control
+        as="select"
+        className="language"
+        onChange={handleLangChange}
+      >
+        <option value="en">English</option>
+        <option value="es">Spanish</option>
+        <option value="de">German</option>
+        <option value="ko">Korean</option>
+        <option value="ar">Arabic</option>
+      </Form.Control>
+    </form>
+  );
+};
 
 export default MessageForm;
